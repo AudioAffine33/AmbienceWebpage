a035dc2361dcad4b22ceaad2c00c0ec0246d216d
AudioAffine33 <SnapJackBlack@markusmacbookpro.fritz.box>
2014-04-21 11:12:48 +0200

    Startseite
    Bilderlinks und .php

---
+++ Startseite.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>AmbienceWebsite_Startseite</title>
+
+	<link rel="stylesheet" href="Startseite_style.css" type="text/css" />
+	
+</head>
+
+<body>
+<div id="Content">
+
+	<div id="Top">
+
+		<div id="TopLeft"><img src="Testbilder/533046l.jpg" class="AmbienceFoto desaturate grey"/></div>
+    
+    	<div id="TopCenter"><img src="Testbilder/dsc_9869axhaf.jpg" class="AmbienceFoto desaturate grey"/></div>
+    
+    	<div id="TopRight"><img src="Testbilder/DSC00904.jpg" class="AmbienceFoto desaturate grey" /></div>
+    
+    </div>
+
+	<div id="Center">
+  
+    	<div id="MidLeft"><img src="Testbilder/dscn1776.jpg" class="AmbienceFoto desaturate grey" /></div>
+    
+   		<div id="LoginBox">
+        	<div id="LoginLeft"> 
+        
+       			<h1>Login</h1> 
+   	
+          	</div>
+        
+        	<div id="LoginRight"> 
+            <h1>Entdecke</h1>
+            
+            </div>
+	</div>
+    
+   	 	<div id="MidRight"><img src="Testbilder/hollywood-walk-of-fame-100.jpg" class="AmbienceFoto desaturate grey" /></div>
+   </div>
+ 
+
+   	<div id="Bot">
+    
+  		<div id="BotLeft"><img src="Testbilder/lanschaftsbilder004.jpg" class="AmbienceFoto desaturate grey" /></div>
+    
+    	<div id="BotCenter" ><img src="Testbilder/New_york_times_square-terabass.jpg" class="AmbienceFoto desaturate grey"/></div>
+    
+    	<div id="BotRight" ><img src="Testbilder/Tokyo_street.jpg" class="AmbienceFoto desaturate grey" /></div>
+	</div>
+
+</div>
+</body>
+
+</html>

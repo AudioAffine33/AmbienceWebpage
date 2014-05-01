d03bc6c071474f16ffa24b0e05d2587d44aa6868
AudioAffine33 <SnapJackBlack@markusmacbookpro.connectify>
2014-05-01 13:09:08 +0200

    Registrierung mit Design

--- register.php
+++ register.php
@@ -2,7 +2,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Unbenanntes Dokument</title>
+<link rel="stylesheet" href="css/Registrierung.css" />
+<title>Registrierung</title>
 
 	<?php
 		include('php/include.php');
@@ -39,38 +40,44 @@
 </head>
 
 <body>
-  	<form method="POST">
-    	<table>
+	<div id="registrierung">
+    <h1>Registrierung</h1>
+  	<div id="felder">
+    <form method="POST">   	
+        <table>
         	<tr>
-            	<td>Name:</td>
+            	<td class="eingabe">Name:</td>
             	<td><input type="text" name="regName" <?php if(isset($errorReg['name'])){ ?> style="background-color:#F00" <?php } ?> /></td>
                 <td><?php if(isset($errorReg['name'])){ echo $errorReg['name']; } ?></td>
         	</tr>
             <tr>
-            	<td>Passwort:</td>
+            	<td class="eingabe">Passwort:</td>
             	<td><input type="password" name="regPass1"<?php if(isset($errorReg['mail'])){ ?> style='background-color:#F00'<?php } ?> /></td>
                 <td><?php if(isset($errorReg['pass'])){ echo $errorReg['pass']; } ?></td>
         	</tr>
             <tr>
-            	<td>Passwort wdh.:</td>
+            	<td class="eingabe">Passwort wdh.:</td>
             	<td><input type="password" name="regPass2"<?php if(isset($errorReg['mail'])){ ?> style='background-color:#F00'<?php } ?> /></td>
         	</tr>
             <tr>
-            	<td>Email:</td>
+            	<td class="eingabe">Email:</td>
             	<td><input type="text" name="regMail"<?php if(isset($errorReg['mail'])){ ?> style='background-color:#F00'<?php } ?> /></td>
                 <td><?php if(isset($errorReg['mail'])){ echo $errorReg['mail']; } ?></td>
         	</tr>
             <tr>
-            	<td colspan="2">
+            	<td colspan="2" class="eingabe">
                 	<?php
 					  echo recaptcha_get_html($publickey);
 					?>
                 </td>
             </tr>
             <tr>
-            	<td colspan="2"><input type="submit" value="Registrieren" /></td>
+            	<td id="regbut"><input type="submit" value="Registrieren" /></td>
+                <td id="abbut"></td>
         	</tr>
         </table>
-	</form>   	
+   	</form> 
+    </div>
+    </div>  	
 </body>
 </html>

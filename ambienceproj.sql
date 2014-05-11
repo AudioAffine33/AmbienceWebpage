0fbb2dc0e87b87c60fb435adbd530c9984555683
ExVision <marco@lunaarte.de>
2014-05-11 18:28:32 +0200

    Noch die aktuelle Datenbank, wer nicht selbst Hochladen und bewerten will

--- ambienceproj.sql
+++ ambienceproj.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 11. Mai 2014 um 11:57
+-- Erstellungszeit: 11. Mai 2014 um 18:26
 -- Server Version: 5.6.16
 -- PHP-Version: 5.5.9
 
@@ -45,14 +45,14 @@ CREATE TABLE IF NOT EXISTS `ambience` (
   `originator` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `filename` (`filename`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=82 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=92 ;
 
 --
 -- Daten für Tabelle `ambience`
 --
 
 INSERT INTO `ambience` (`id`, `format_id`, `filename`, `size`, `length`, `name`, `user_id`, `location_id`, `date`, `time`, `description`, `category_id`, `picture`, `rating`, `date_added`, `originator`) VALUES
-(13, 36, '13_birds_various_28_01_birds__various_count.wav', 29520914, 150, 'BIRDS VARIOUS', 4, 52, '2005-08-04', '01:00:00', 'BIRDS, VARIOUS COUNTRY AMBIENCE, EARLY MORNING, RESIDENTIAL', 2, '13_birds_various_28_01_birds__various_count.jpg', NULL, '2014-04-22', 'NetMixPro'),
+(13, 36, '13_birds_various_28_01_birds__various_count.wav', 29520914, 150, 'BIRDS VARIOUS', 4, 52, '2012-09-04', '01:00:00', 'BIRDS, VARIOUS COUNTRY AMBIENCE, EARLY MORNING, RESIDENTIAL', 2, '13_birds_various_28_01_birds__various_count.jpg', NULL, '2014-04-22', 'NetMixPro'),
 (14, 36, '14_city_heavy_17_city__heavy_traffic_and_ci.wav', 46492132, 242, 'City Heavy', 4, 53, '2005-05-02', '01:00:00', 'CITY, HEAVY TRAFFIC AND CITY RUMBLE FROM BALCONY AMBIENCE', 1, '14_city_heavy_17_city__heavy_traffic_and_ci.jpg', NULL, '2014-04-22', 'NetMixPro'),
 (16, 36, '16_city_medium_06_city__medium_traffic_and_.wav', 49466226, 244, 'City Medium', 4, 54, '2005-05-02', '12:00:00', 'CITY, MEDIUM TRAFFIC AND PEDESTRIANS, CITY RUMBLE AMBIENCE', 1, '16_city_medium_06_city__medium_traffic_and_.jpg', NULL, '2014-04-22', 'NetMixPro'),
 (17, 37, '17_country_ambience_06_country__ambience_co.wav', 6770716, 69, 'Country Ambience', 4, 55, '2005-05-02', '01:00:00', 'COUNTRY, AMBIENCE COUNTRY: BIRDS, COWS, ROOSTER, TRACTOR IN B/G', 2, '17_country_ambience_06_country__ambience_co.jpg', NULL, '2014-04-22', 'NetMixPro'),
@@ -145,7 +145,7 @@ CREATE TABLE IF NOT EXISTS `location` (
   `longitude` decimal(18,14) NOT NULL,
   `countrycode` varchar(5) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=69 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=76 ;
 
 --
 -- Daten für Tabelle `location`
@@ -162,8 +162,11 @@ INSERT INTO `location` (`id`, `continent`, `name`, `land`, `latitude`, `longitud
 (56, 'Europe', 'Nürnberg', 'Deutschland', '49.45203000000000', '11.07675000000000', 'DE'),
 (57, 'North America', 'Dallas', 'USA', '32.78013990000000', '-96.80045110000000', 'US'),
 (61, 'Europe', 'Brüssel', 'Belgien', '50.85033960000000', '4.35171030000000', 'BE'),
-(67, 'Asia', 'Unknown', 'Japan', '35.68948750000000', '139.69170640000000', 'JP'),
-(68, 'Asia', 'Tokio', 'Japan', '35.68948750000000', '139.69170640000000', 'JP');
+(68, 'Asia', 'Tokio', 'Japan', '35.68948750000000', '139.69170640000000', 'JP'),
+(72, 'Europe', 'Nürnberg', 'Deutschland', '49.45203000000000', '11.07675000000000', 'DE'),
+(73, 'Europe', 'Amberg', 'Deutschland', '49.44031980000000', '11.86334450000000', 'DE'),
+(74, 'Europe', 'Xanten', 'Deutschland', '51.65710810000000', '6.44865040000000', 'DE'),
+(75, 'Europe', 'Aschaffenburg', 'Deutschland', '49.98066250000000', '9.13555539999990', 'DE');
 
 -- --------------------------------------------------------
 
@@ -178,7 +181,18 @@ CREATE TABLE IF NOT EXISTS `rating` (
   `rating` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id` (`user_id`,`ambience_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Daten für Tabelle `rating`
+--
+
+INSERT INTO `rating` (`id`, `user_id`, `ambience_id`, `rating`) VALUES
+(1, 4, 14, 3),
+(2, 3, 14, 3),
+(3, 5, 14, 5),
+(4, 5, 13, 1),
+(5, 5, 81, 5);
 
 -- --------------------------------------------------------
 
@@ -214,14 +228,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `emailShown` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
 --
 -- Daten für Tabelle `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `pass`, `email`, `about`, `picture`, `rights`, `emailShown`) VALUES
-(3, 'Marco', 'f5888d0bb58d611107e11f7cbc41c97a', 'marco@exvision.de', 'Lorem Ipsum', '', 'admin', 0),
+(3, 'Marco', 'f5888d0bb58d611107e11f7cbc41c97a', 'marco@exvision.de', 'Lorem Ipsum', '3_Marco.jpg', 'admin', 0),
 (4, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@ambie.nce', 'Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that''s what you see at a toy store. And you must think you''re in a toy store, because you''re here shopping for an infant named Jeb.', '', 'admin', 0),
 (5, 'sebastian', 'c2d628ba98ed491776c9335e988e2e3b', 'sebastian@sebastian.de', 'wort tulip glass saccharification aerobic. grainy, all-malt krausen additive primary fermentation; mash length lagering aau alcohol goblet. shelf life; enzymes cold filter krausen, " trappist acid rest grainy hydrometer enzymes balthazar." balthazar, malt lauter sparge specific gravity lambic length. draft (draught) carboy grainy alpha acid seidel, units of bitterness? grainy filter ale anaerobic balthazar; carboy terminal gravity wort alpha acid. units of bitterness bottle conditioning; mead kr', NULL, 'user', 0),
 (6, 'markus', '23c496d2ee2494b3f380a2bd7380b811', 'markus@markus.de', 'Zombie ipsum brains reversus ab cerebellum viral inferno, brein nam rick mend grimes malum cerveau cerebro. De carne cerebro lumbering animata cervello corpora quaeritis. Summus thalamus brains sit​​, morbo basal ganglia vel maleficia? De braaaiiiins apocalypsi gorger omero prefrontal cortex undead survivor fornix dictum mauris. Hi brains mindless mortuis limbic cortex soulless creaturas optic nerve, imo evil braaiinns stalking monstra hypothalamus adventus resi hippocampus dentevil vultus brain', NULL, 'user', 0);
